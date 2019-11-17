23d623211b418c1a19feace27778e8eb765f05ca
root <root@localhost.localdomain>
2019-11-17 11:37:40 +0000

    Added login functionality partially. [Sessions are stored, passwords are not encrypted and the login really does nothing at the moment it just stores your session id], Reigistration is fully functional with flash messages to be displayed when there are errors. Flash messages are not styled currently. Will most likely use bootstrap to quickly style it

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -3,15 +3,34 @@ class UsersController < ApplicationController
     	puts "Index"
     end
     def create 
-    	logger.debug "Test"
-    	userExists("asd")
-    	# puts "Hii"
-    	# user_infohash = params[:user];
+    	@error = "";
+    	# Getting the parameters
+    	user_infohash = params[:user];
+    	#Storing paramters in variables for easy access
+    	email = user_infohash[:email];
+    	password = user_infohash[:password];
+    	confirm_password = user_infohash[:password2]
 
-    	# @user = User.new({:email => user_infohash[:email], :password => user_infohash[:email], :admin => false});
-    	# if(@user.save)
-    	# 	puts "User created successfully: ";
-    	# end
+    	# Checking if the user does not exist and if the passwords are equal and if all the fields have more than 5 characters;
+    	if( !(userExists(email)) and checkEquality(password,confirm_password) and checkValidity(email,"Email") and checkValidity(password,"Password") and checkValidity(confirm_password, "Confirm Password")  )
+    		#Creating the user
+
+    		 @user = User.new({:email => user_infohash[:email], :password => user_infohash[:email], :admin => false});
+    		 # If the user is successfully created
+    		 if(@user.save)
+    		 	logger.debug "User created"
+    		 	session[:user_id] = @user.id;
+    		 	#Add session here
+    		 else
+    		 	#Flash Error
+    		 end
+    	else
+    		#Flash Error Email exists or password fields are not the same or less than 5 characters
+    		flash[:error] = @error
+    		redirect_to '/login'
+
+    	end
+
     end
 
 
@@ -19,13 +38,36 @@ class UsersController < ApplicationController
    private def parameter
    end
 
-   private def userExists(email_data)
-   	found = User.where(email: email_data)
-   	if(found != nil)
-   		puts "Account found"
+   private def checkValidity(entry,type)
+   	if(entry.mb_chars.length > 5)
+   		return true;
    	else
-   		puts "NOT EXISTS"
+   		@error = @error + type + "Field must be greating than 5 characters"
    	end
+   	return false;
+   end
+
+
+
+   private def checkEquality(password, password2)
+   	if(password2 == password)
+   		return true;
+   	else
+   		@error = @error + " Password fields are not the same \n"
+   	end
+   	return false;
+   end
+
+
+
+   private def userExists(email_data)
+   	t = User.find_by(email: email_data) 
+   	if(User.find_by(email: email_data) == nil)
+   		return false
+   	else
+   		@error = @error + " Email is already in use\n"
+   	end
+   	return true;
    end
     
 end
