49309d0c1642d9851f5c05676cfc4ddf0867ac5e
Mahdi <mahdi_moh@outlook.com>
2019-12-16 13:03:16 +0000

    carousel fixed

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -51,88 +51,88 @@ $(document).ready(function() {
 
 })
 
-// function previous_carousel(){
+function previous_carousel(){
 
-// 	if(((count - 1) < 0)){
-// 		count = ($(".slide").length);
-// 		$(".slide").each(function(i){
-// 			if(i == (count - 1)){
-// 				$(this).attr("style","display:block;")
-// 			}else{
-// 				$(this).attr("style","display:none;")
+	if(((count - 1) < 0)){
+		count = ($(".slide").length);
+		$(".slide").each(function(i){
+			if(i == (count - 1)){
+				$(this).attr("style","display:block;")
+			}else{
+				$(this).attr("style","display:none;")
 
-// 			}
-// 		})
-// 		count = count - 1;
+			}
+		})
+		count = count - 1;
 
-// 	}else{
-// 		$(".slide").each(function(i){
-// 			if(i == (count-1)){
-// 				$(this).attr("style","display:block;")
-// 			}else{
-// 				$(this).attr("style","display:none;")
+	}else{
+		$(".slide").each(function(i){
+			if(i == (count-1)){
+				$(this).attr("style","display:block;")
+			}else{
+				$(this).attr("style","display:none;")
 
-// 			}
+			}
 
-// 		})
-// 		count = count - 1;
-// 	}
-// 		console.log("COUNTER " + count)
+		})
+		count = count - 1;
+	}
+		console.log("COUNTER " + count)
 
-// }
-// function next_carousel(){
-// 	console.log(($(".slide").length - 1))
-// 	if(count > ($(".slide").length -1)){
-// 		count = 0
-// 		$(".slide").each(function(i){
-// 			if(i == (count)){
-// 				$(this).attr("style","display:block;")
-// 			}else{
-// 				$(this).attr("style","display:none;")
+}
+function next_carousel(){
+	console.log(($(".slide").length - 1))
+	if(count > ($(".slide").length -1)){
+		count = 0
+		$(".slide").each(function(i){
+			if(i == (count)){
+				$(this).attr("style","display:block;")
+			}else{
+				$(this).attr("style","display:none;")
 
-// 			}
-// 		})
-// 	}else{
-// 		$(".slide").each(function(i){
-// 		count = count + 1
+			}
+		})
+	}else{
+		$(".slide").each(function(i){
+		count = count + 1
 
-// 		if(i == (count)){
-// 			$(this).attr("style","display:block;")
-// 		}else{
-// 			$(this).attr("style","display:none;")
+		if(i == (count)){
+			$(this).attr("style","display:block;")
+		}else{
+			$(this).attr("style","display:none;")
 
-// 		}
-// 		})
+		}
+		})
 
-// 	}
-// 	console.log("COUNTER " + count)
+	}
+	console.log("COUNTER " + count)
 
-// }
-// function next_carousel(){
-// 	console.log(($(".slide").length - 1))
-// 	if(count > ($(".slide").length -2)){
-// 		count = 0
-// 		if(i == (count)){
-// 			$(this).attr("style","display:block;")
-// 		}else{
-// 			$(this).attr("style","display:none;")
+}
+function next_carousel(){
+	console.log(($(".slide").length - 1))
+	if(count > ($(".slide").length -2)){
+		count = 0
+		if(i == (count)){
+			$(this).attr("style","display:block;")
+		}else{
+			$(this).attr("style","display:none;")
 
-// 		}
-// 	}else{
-// 		count = count + 1
-// 		$(".slide").each(function(i){
-// 			console.log("I : " + i)
-// 			if(i == (count)){
-// 				$(this).attr("style","display:block;")
-// 			}else{
-// 				$(this).attr("style","display:none;")
-// 			}
-// 		})
+		}
+	}else{
+		count = count + 1
+		$(".slide").each(function(i){
+			console.log("I : " + i)
+			if(i == (count)){
+				$(this).attr("style","display:block;")
+			}else{
+				$(this).attr("style","display:none;")
+			}
+		})
 
-// 	}
-// 	console.log("COUNTER " + count)
+	}
+	console.log("COUNTER " + count)
 
-// }
+}
 function hide(index){
 	let x = 0;
 	// Getting all the description cards.
--- app/models/post.rb
+++ app/models/post.rb
@@ -5,5 +5,6 @@ class Post < ApplicationRecord
   # validates :body, length: { minimum: 10 }
   has_attached_file :banner, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
   validates_attachment_content_type :banner, content_type: /\Aimage\/.*\z/
+  validates_presence_of :title,:body,:summary
   #validates_presence_of :title,:body,:summary, :banner
 end
